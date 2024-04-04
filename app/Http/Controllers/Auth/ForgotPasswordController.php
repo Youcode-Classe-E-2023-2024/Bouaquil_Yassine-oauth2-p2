72a6a8b509699ee81ca84d45919c2a9eaab1a5ae
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-04-04 09:24:56 +0000

    ROLE & Permission

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -6,33 +6,39 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 
-
 class ForgotPasswordController extends Controller
 {
     /**
-     * Handle a reset password email request for the application.
+     * Gère la demande d'envoi d'e-mail de réinitialisation de mot de passe pour l'application.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function sendResetLinkEmail(Request $request){
+        // Valider les données de la requête
         $request->validate(['email'=>'required|email']);
 
+        // Envoyer le lien de réinitialisation de mot de passe via le broker Password
         $response=$this->broker()->sendResetLink(
             $request->only('email')
         );
 
+        // Vérifier si le lien de réinitialisation a été envoyé avec succès ou non
         return $response==Password::RESET_LINK_SENT
-            ? response()->json(['message' => 'Reset link sent to your email.'])
-            : response()->json(['message' => 'Unable to send reset link'], 500);
+            ? response()->json(['message' => 'Reset link sent to your email.']) // Si réussi
+            : response()->json(['message' => 'Unable to send reset link'], 500); // Si échec avec code d'erreur 500
     }
+
     /**
-     * Get the broker to be used during password reset.
+     * Récupère le broker à utiliser lors de la réinitialisation du mot de passe.
      *
      * @return \Illuminate\Contracts\Auth\PasswordBroker
      */
+
+
     public function broker()
     {
+        // Retourne le broker Password pour gérer la réinitialisation du mot de passe
         return Password::broker();
     }
 
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,9 +27,28 @@ Route::middleware('api')->group(function () {
 });
 
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum','role:admin'])->group(function () {
+
+    Route::post('/users/{user}/assign-role', [UserController::class, 'assignRole']);
+
+
     Route::get('/users', [UserController::class, 'index']);
     Route::post('/users', [UserController::class, 'store']);
     Route::put('/users/{user}', [UserController::class, 'update']);
     Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
+
+    Route::get('/roles', [RoleController::class, 'index']);
+    Route::post('/roles', [RoleController::class, 'store']);
+    Route::put('/roles/{role}', [RoleController::class, 'update']);
+    Route::delete('/roles/{role}', [RoleController::class, 'destroy']);
+
+
+    Route::get('/permissions', [PermissionController::class, 'index']);
+    Route::post('/permissions', [PermissionController::class, 'store']);
+    Route::put('/permissions/{permissions}', [PermissionController::class, 'update']);
+    Route::delete('/permissions/{permissions}', [PermissionController::class, 'destroy']);
+
+
+    Route::post('/roles/{role}/permissions', [RoleController::class, 'assignPermissions']);
 });
